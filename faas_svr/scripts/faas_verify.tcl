26e3cbba008dc569a250256408eb54d5a0a626f0
Chandler Zhao <zhao_chunxiao@hotmail.com>
2020-07-28 05:34:25 -0400

    update sever site script, for pr_verify and xclbin generating

--- faas_svr/scripts/faas_verify.tcl
+++ faas_svr/scripts/faas_verify.tcl
@@ -1,11 +1,17 @@
 
-set status [pr_verify -initial ../golden_design/golden.dcp -additional kernel.dcp]
+pr_verify -initial ../golden_design/golden.dcp -additional ./kernal.dcp -file chk_result.txt
 
-if {status} {
+set pr_rslt [read [open "chk_result.txt" r]]
+set status [regex -line "^INFO:.*PR_VERIFY.*are compatible" $pr_rslt]
+
+if {$status} {
    #process bit gen
-   open_checkpoint kernel.dcp
-   write_bitstream -f kernel.bit
-   }
-else {
-   exit
+   puts "generate kernal bitstream"
+   open_checkpoint kernal.dcp
+   write_bitstream -f kernal.bit
+   puts "INFO: kernal bitstream is generated."
+   exit 0
+   } else {
+   puts "Error: pr_verify Failed. No bitstream generated"
+   exit 1
 }
--- faas_svr/scripts/xclbin_proc.sh
+++ faas_svr/scripts/xclbin_proc.sh
@@ -1,5 +1,14 @@
-xclbinutil --dump-section BITSTREAM:DCP:kernel.dcp --input \1.xclbin
+#!/usr/bin/bash
+# usage
+#  cmd: xclbin_pro.sh <xclbin file name, without postfix xclbin>
+#  output <orig name>_dummy.xclbin : the xclbin which contains meta_data only
+#  output <orig name>_bit.xclbin : the xclbin which contains meta_data and bit file
+#  
+xclbinutil --dump-section BITSTREAM:RAW:kernel.dcp --input $1.xclbin
 
 vivado -mode tcl -source faas_verify.tcl
 
+ xclbinutil --remove-section BITSTREAM --input $1.xclbin  --output $1_dummy.xclbin
+
+xclbinutil --add-section BITSTREAM:RAW:kernal.bit --input $1_dummy.xclbin --output $1_bit.xclbin
 
