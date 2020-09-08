3330a328813c3dbbff1de7f7c38d0ab5a2fda491
Chandler Zhao <chunxia@xilinx.com>
2020-09-08 08:28:41 -0600

    update scripts, use partial bit for xclbin file

--- faas_svr/scripts/faas_verify.tcl
+++ faas_svr/scripts/faas_verify.tcl
@@ -1,5 +1,5 @@
 
-pr_verify -initial ../golden_design/golden.dcp -additional ./kernal.dcp -file chk_result.txt
+pr_verify -initial ../../golden_design/golden.dcp -additional ./kernal.dcp -file chk_result.txt
 
 set pr_rslt [read [open "chk_result.txt" r]]
 set status [regex -line "^INFO:.*PR_VERIFY.*are compatible" $pr_rslt]
@@ -8,7 +8,11 @@ if {$status} {
    #process bit gen
    puts "generate kernal bitstream"
    open_checkpoint kernal.dcp
-   write_bitstream -f kernal.bit
+   
+   set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
+   
+   #write_bitstream -cell [get_cells pfm_top_i/dynamic_region] -f kernal.bit
+   write_bitstream -force kernal.bit
    puts "INFO: kernal bitstream is generated."
    exit 0
    } else {
--- faas_svr/scripts/xclbin_proc.sh
+++ faas_svr/scripts/xclbin_proc.sh
@@ -4,11 +4,41 @@
 #  output <orig name>_dummy.xclbin : the xclbin which contains meta_data only
 #  output <orig name>_bit.xclbin : the xclbin which contains meta_data and bit file
 #  
-xclbinutil --dump-section BITSTREAM:RAW:kernel.dcp --input $1.xclbin
 
-vivado -mode tcl -source faas_verify.tcl
+if  [ $# != 1 ]
+then
+  echo "Usage: xclbin_proc.sh <kernal name>.xclbin"
+  exit
+fi
 
- xclbinutil --remove-section BITSTREAM --input $1.xclbin  --output $1_dummy.xclbin
+if [[ "$1" =~ .*xclbin$ ]]
+then
+  xcl_file=$1
+else
+  xcl_file=$1.xclbin
+fi
 
-xclbinutil --add-section BITSTREAM:RAW:kernal.bit --input $1_dummy.xclbin --output $1_bit.xclbin
+rm -rf kernal*.bit
+rm -rf kernal.dcp
 
+xclbinutil --dump-section BITSTREAM:RAW:kernal.dcp --input $xcl_file
+
+
+vivado -mode tcl -source ../scripts/faas_verify.tcl
+
+xclbinutil --remove-section BITSTREAM --input $1.xclbin  --output $1_dummy.xclbin
+
+if grep -q "^INFO:.*PR_VERIFY.*are compatible" chk_result.txt
+then
+  partial_bit=`find . -name kernal*partial.bit`
+  if   [ ! -z $partial_bit ]
+  then
+
+     #xclbinutil --add-section BITSTREAM:RAW:kernal_pblock_region_partial.bit --input $1_dummy.xclbin --output $1_bit.xclbin
+     xclbinutil --add-section BITSTREAM:RAW:$partial_bit --input $1_dummy.xclbin --output $1_bit.xclbin
+  else
+    echo "Error: Partial bit file is null!"  
+  fi
+else
+  echo "Error: pr_verify Failed!"
+fi
